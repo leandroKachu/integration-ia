c1f586b7b8d89c7b89ae6584023331a945c3b694
leandro gaia <lesilvajunior@hotmail.com>
2026-01-06 10:52:32 -0300

    add gemini test

---
+++ run-ia-google.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from google import genai
+
+import os
+
+load_dotenv()
+key = os.getenv("KEY_API")
+print(key)
+
+client = genai.Client(api_key=key)
+while True:
+    values = input("Digite algo (enter vazio para sair): ")
+    
+    if values == "sair" or values == "exit": 
+        print( "obrigado até ")
+        break
+
+    if values == "":
+        break
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=values,
+    )
+    print(response.text)
+
+
+
+
